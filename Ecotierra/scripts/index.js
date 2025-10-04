cf8642137e80c5d015dcbcd6e90c06c4c879cd33
jazminruiz16 <jazmingruiz@gmail.com>
2025-10-04 15:19:13 -0400

    wireframe

---
+++ Ecotierra/scripts/index.js
@@ -0,0 +1,161 @@
+
+console.dir(document);
+const currentyear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentyear;
+const lastModified = document.lastModified;
+const date = new Date(lastModified);
+document.getElementById("lastModified").textContent = date;
+
+const button = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+const header = document.querySelector('header');
+const listPhotos = document.getElementById('listPhotos');
+
+
+button.addEventListener("click", function () {
+    menu.classList.toggle('open');
+    if (button.innerHTML === '☰') {
+        button.textContent = '❌';
+        header.style.height = '300px';
+
+    }
+    else {
+        button.textContent = '☰';
+        header.style.height = '150px';
+
+    }
+
+});
+const gridLink = document.getElementById('grid');
+const listLink = document.getElementById('list');
+gridLink.addEventListener("click", async function () {
+    listPhotos.innerHTML = '';
+    try {
+        const response = await fetch("https://raw.githubusercontent.com/jazminruiz16/wdd231/main/chamber/members.json"); // Wait for the fetch to complete 
+        const data = await response.json(); // Wait for the response to be converted to JSON
+        // Output the fetched data
+        data.industries.forEach(industries => {
+            const listItem = document.createElement("figure");
+            listItem.innerHTML = '<div><figcaption><span class="titlePhoto">' + industries.name + '</span><br>Adress: ' + industries.adress + '<br>Phone number: ' + industries.phonenumber + '<br>Membership level: ' + industries.membershiplevel + '<br>Information: ' + industries.information + ' </figcaption><img src="' + industries.imageurl + '" alt="' + industries.name + '" width=250 loading="lazy"></div>';
+            listPhotos.appendChild(listItem);
+        });
+
+    } catch (error) {
+        console.error("Error fetching data:", error); // Handle any errors
+    }
+    listPhotos.classList.add("gridclass");
+    listPhotos.classList.add("directorygrid");
+    listPhotos.classList.remove("listclass");
+});
+listLink.addEventListener("click", async function () {
+    listPhotos.innerHTML = '';
+    try {
+        const response = await fetch("https://raw.githubusercontent.com/jazminruiz16/wdd231/main/chamber/members.json"); // Wait for the fetch to complete 
+        const data = await response.json(); // Wait for the response to be converted to JSON
+        // Output the fetched data
+        data.industries.forEach(industries => {
+            const listItem = document.createElement("div");
+            listItem.innerHTML = '<p>' + industries.name + '</p><p>' + industries.adress + '</p><p>' + industries.phonenumber + '</p><p>MSL: ' + industries.membershiplevel + '</p><p>info.: ' + industries.information + '</p>';
+            listPhotos.appendChild(listItem);
+        });
+
+    } catch (error) {
+        console.error("Error fetching data:", error); // Handle any errors
+    }
+    listPhotos.classList.add("listclass");
+    listPhotos.classList.remove("gridclass");
+    listPhotos.classList.remove("directorygrid");
+
+
+
+});
+const fetchData = async () => {
+    listPhotos.innerHTML = '';
+    try {
+        const response = await fetch("https://raw.githubusercontent.com/jazminruiz16/wdd231/main/chamber/members.json"); // Wait for the fetch to complete 
+        const data = await response.json(); // Wait for the response to be converted to JSON
+        // Output the fetched data
+        data.industries.forEach(industries => {
+            const listItem = document.createElement("figure");
+            listItem.innerHTML = '<div><figcaption><span class="titlePhoto">' + industries.name + '</span><br>Adress: ' + industries.adress + '<br>Phone number: ' + industries.phonenumber + '<br>Membership level: ' + industries.membershiplevel + '<br>Information: ' + industries.information + ' </figcaption><img src="' + industries.imageurl + '" alt="' + industries.name + '" width=250 loading="lazy"></div>';
+            listPhotos.appendChild(listItem);
+        });
+
+    } catch (error) {
+        console.error("Error fetching data:", error); // Handle any errors
+    }
+};
+fetchData();
+
+const form = document.querySelector("#form");
+form.addEventListener("submit", () => {
+    const date = new Date();
+    document.getElementById("timestamp").value = date;
+});
+
+/*const oldLink = document.getElementById('old');
+const newLink = document.getElementById('new');
+const largeLink = document.getElementById('large');
+const smallLink = document.getElementById('small');
+const homeLink = document.getElementById('home');
+oldLink.addEventListener("click", function () {
+    let old = temples.filter(temple => {
+        const year = parseInt(temple.dedicated.split(",")[0]);
+        return year < 2000;
+    });
+    listPhotos.innerHTML = '';
+    old.forEach(temple => {
+        const listItem = document.createElement("figure");
+        listItem.innerHTML = '<figcaption><span class="titlePhoto">' + temple.templeName + '</span><br>LOCATION: ' + temple.location + '<br>DEDICATED: ' + temple.dedicated + '<br>SIZE: ' + temple.area + ' sq ft</figcaption><img src="' + temple.imageUrl + '" alt="' + temple.templeName + '" loading="lazy">';
+        listPhotos.appendChild(listItem);
+    });
+});
+newLink.addEventListener("click", function () {
+    let newest = temples.filter(temple => {
+        const year = parseInt(temple.dedicated.split(",")[0]);
+        return year >= 2000;
+    });
+    listPhotos.innerHTML = '';
+    newest.forEach(temple => {
+        const listItem = document.createElement("figure");
+        listItem.innerHTML = '<figcaption><span class="titlePhoto">' + temple.templeName + '</span><br>LOCATION: ' + temple.location + '<br>DEDICATED: ' + temple.dedicated + '<br>SIZE: ' + temple.area + ' sq ft</figcaption><img src="' + temple.imageUrl + '" alt="' + temple.templeName + '" loading="lazy">';
+        listPhotos.appendChild(listItem);
+    });
+});
+largeLink.addEventListener("click", function () {
+    let large = temples.filter(temple => {
+        const area = temple.area;
+        return area >= 90000;
+    });
+    listPhotos.innerHTML = '';
+    large.forEach(temple => {
+        const listItem = document.createElement("figure");
+        listItem.innerHTML = '<figcaption><span class="titlePhoto">' + temple.templeName + '</span><br>LOCATION: ' + temple.location + '<br>DEDICATED: ' + temple.dedicated + '<br>SIZE: ' + temple.area + ' sq ft</figcaption><img src="' + temple.imageUrl + '" alt="' + temple.templeName + '" loading="lazy">';
+        listPhotos.appendChild(listItem);
+    });
+});
+smallLink.addEventListener("click", function () {
+    let small = temples.filter(temple => {
+        const area = temple.area;
+        return area <= 10000;
+    });
+    listPhotos.innerHTML = '';
+    small.forEach(temple => {
+        const listItem = document.createElement("figure");
+        listItem.innerHTML = '<figcaption><span class="titlePhoto">' + temple.templeName + '</span><br>LOCATION: ' + temple.location + '<br>DEDICATED: ' + temple.dedicated + '<br>SIZE: ' + temple.area + ' sq ft</figcaption><img src="' + temple.imageUrl + '" alt="' + temple.templeName + '" loading="lazy">';
+        listPhotos.appendChild(listItem);
+    });
+});
+homeLink.addEventListener("click", function () {
+    listPhotos.innerHTML = '';
+    temples.forEach(temple => {
+        const listItem = document.createElement("figure");
+        listItem.innerHTML = '<figcaption><span class="titlePhoto">' + temple.templeName + '</span><br>LOCATION: ' + temple.location + '<br>DEDICATED: ' + temple.dedicated + '<br>SIZE: ' + temple.area + ' sq ft</figcaption><img src="' + temple.imageUrl + '" alt="' + temple.templeName + '" loading="lazy">';
+        listPhotos.appendChild(listItem);
+    });
+});
+const figureTemple = temples.map((temple) => {
+    const listItem = document.createElement("figure");
+    listItem.innerHTML = '<figcaption><span class="titlePhoto">' + temple.templeName + '</span><br>LOCATION: ' + temple.location + '<br>DEDICATED: ' + temple.dedicated + '<br>SIZE: ' + temple.area + ' sq ft</figcaption><img src="' + temple.imageUrl + '" alt="' + temple.templeName + '" loading="lazy">';
+    listPhotos.appendChild(listItem);
+});*/
---
+++ Ecotierra/scripts/modal.js
@@ -0,0 +1,43 @@
+const modal1 = document.querySelector('#myModalnormal');
+const openModal = document.querySelector('#openmodal1');
+const closeModal = document.querySelector('#closeModal1');
+
+openModal.addEventListener('click', () => {
+    modal1.show();
+});
+closeModal.addEventListener('click', () => {
+    modal1.close();
+});
+
+const modal2 = document.querySelector('#myModalbronze');
+const openModal2 = document.querySelector('#openmodal2');
+const closeModal2 = document.querySelector('#closeModal2');
+
+openModal2.addEventListener('click', () => {
+    modal2.show();
+});
+closeModal2.addEventListener('click', () => {
+    modal2.close();
+});
+
+const modal3 = document.querySelector('#myModalsilver');
+const openModal3 = document.querySelector('#openmodal3');
+const closeModal3 = document.querySelector('#closeModal3');
+
+openModal3.addEventListener('click', () => {
+    modal3.show();
+});
+closeModal3.addEventListener('click', () => {
+    modal3.close();
+});
+
+const modal4 = document.querySelector('#myModalgolden');
+const openModal4 = document.querySelector('#openmodal4');
+const closeModal4 = document.querySelector('#closeModal4');
+
+openModal4.addEventListener('click', () => {
+    modal4.show();
+});
+closeModal4.addEventListener('click', () => {
+    modal4.close();
+});
