7382744a58f53673088875ae9361daf732d7d51d
jazminruiz16 <jazmingruiz@gmail.com>
2025-10-06 19:47:42 -0400

    errors

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,3 +1,9 @@
+console.dir(document);
+const currentyear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentyear;
+const lastModified = document.lastModified;
+const date = new Date(lastModified);
+document.getElementById("lastModified").textContent = date;
 import { places } from "../data/places.mjs"; 
 console.log(places);
 const showHere = document.getElementById("allplaces");
