2807886f092022b8e63745f341eea0190072780a
jazminruiz16 <jazmingruiz@gmail.com>
2025-10-15 22:59:28 -0400

    fix products

--- Ecotierra/scripts/products.js
+++ Ecotierra/scripts/products.js
@@ -24,49 +24,7 @@ button.addEventListener("click", function () {
     }
 
 });
-/*DISCOUNTS */
 
-
-
-/*const myURL = "https://api.openweathermap.org/data/2.5/weather?lat=-16.50&lon=-68.13&appid=5562c17f0bed0ff43cdc29aab205c9a6&units=metric";
-const apiFetch = async () => {
-    weather.innerHTML = '';
-    try {
-        const response = await fetch(myURL); // Wait for the fetch to complete https://openweathermap.org/img/wn/' + data.weather.icon + '@2x.png
-        const data = await response.json(); // Wait for the response to be converted to JSON https://openweathermap.org/img/wn/10d@2x.png
-        // Output the fetched data
-        const date = new Date();
-        const month = date.getMonth();
-        let season = "Winter";
-        let imageseason = "images/winter.svg";
-        if (month >= 8 && month < 11) {
-            season = "Spring";
-            imageseason = "images/flower.svg";
-
-        }
-        else if (month == 11 || (month >= 0 && month < 2)) {
-            season = "Summer";
-            imageseason = "images/sun.svg";
-        }
-        else if (month >= 2 && month < 5) {
-            season = "Autumn";
-            imageseason = "images/autumn.svg";
-        }
-        else {
-            season = "Winter";
-            imageseason = "images/winter.svg";
-        }
-        const listItem = document.createElement("div");
-        listItem.innerHTML = '<figure><img src="https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png" alt="' + data.weather[0].description + '" width=150 loading="lazy"><figcaption><span class="titlePhoto">Curret Weather</span><br>Temperature: ' + data.main.temp + ' ºC<br>' + data.weather[0].description + '<br>High: ' + data.main.temp_max + ' ºC<br>Low: ' + data.main.temp_min + ' ºC<br>Humidity: ' + data.main.humidity + '%<br>Sunrise: ' + data.sys.sunrise + 'am<br>Sunset: ' + data.sys.sunset + ' pm</figcaption></figure><figure><img src="' + imageseason + '" alt="image season" width=150 loading="lazy"><figcaption><span class="titlePhoto">' + season + '</span><br>Organic farming is seasonal because it is related to the cycles of nature and the rhythms of the earth.</figcaption></figure>';
-        weather.appendChild(listItem);
-
-
-    } catch (error) {
-        console.error("Error fetching data:", error); 
-    }
-};
-apiFetch();*/
-/*LIST OF PRODUCTS*/
 const products = [
     {
         productName: "Celery",
@@ -178,17 +136,6 @@ const products = [
     // Add more product objects here... 
 ];
 
-button.addEventListener("click", function () {
-    menu.classList.toggle('open');
-    console.log(menu);
-    if (button.innerHTML === '☰') {
-        button.textContent = '❌';
-    }
-    else {
-        button.textContent = '☰';
-    }
-
-});
 const vegetablesLink = document.getElementById('vegetables');
 const careLink = document.getElementById('care');
 const productLink = document.getElementById('products');
