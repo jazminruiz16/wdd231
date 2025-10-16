e6efd26f3f932b4870f445fb2cc069c82b2c9c19
jazminruiz16 <jazmingruiz@gmail.com>
2025-10-16 00:35:52 -0400

    about

--- Ecotierra/scripts/modified.js
+++ Ecotierra/scripts/modified.js
@@ -4,3 +4,24 @@ document.getElementById("currentyear").textContent = currentyear;
 const lastModified = document.lastModified;
 const date = new Date(lastModified);
 document.getElementById("lastModified").textContent = date;
+
+const button = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+const header = document.querySelector('header');
+
+
+
+button.addEventListener("click", function () {
+    menu.classList.toggle('open');
+    if (button.innerHTML === '☰') {
+        button.textContent = '❌';
+        header.style.height = '300px';
+
+    }
+    else {
+        button.textContent = '☰';
+        header.style.height = '150px';
+
+    }
+
+});
